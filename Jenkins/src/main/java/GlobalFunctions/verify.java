1d2b17acffd7293dfb003c63f8248a8b78e5b203
srinath <srinath@localhost>
2020-08-02 19:42:19 +0530

    Second commit from slave

---
+++ Jenkins/src/main/java/GlobalFunctions/verify.java
@@ -0,0 +1,10 @@
+package GlobalFunctions;
+
+public class verify {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Changes in slave");
+	}
+
+}
